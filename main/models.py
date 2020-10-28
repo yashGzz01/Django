50c3a1c27a53ddbc6137da994779a5849fa1a367
Yash <yashgoyal2910786@gmail.com>
2020-10-28 14:03:26 +0700

    add ckeditor

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=300)
-    content = models.TextField()
+    content = RichTextField()
     posted_date = models.DateTimeField(auto_now_add=True)
     
